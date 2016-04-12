50a92a55695f54e0ae19ca703bdeaae9fded06d4
dustymc <dustymc@gmail.com>
2016-04-11 21:19:06 -0700

    Signed-off-by: dustymc <dustymc@gmail.com>

--- views/map_specimen_event.sql
+++ views/map_specimen_event.sql
@@ -49,14 +49,53 @@ guid like 'UAMb:Herb%' having count(*) > 1 group by flat.collection_object_id,sp
 
 
 
+
+
+
+
+
+
+
+
+/*
+
+
+ORDERING:
+
+1) place of manufacture (https://github.com/ArctosDB/arctos/issues/862)
+2) not-unaccepted with coordinates
+3) not-unaccepted without coordiantes
+4) whatever's left
+
+
+*/
+
+
+drop view mse_pom;
 drop view mse_nua_coord;
 drop view mse_nua_nocoord;
 drop view mse_therest;
 
 
--- not-unaccepted with coordinates
+
+-- 1: place of manufacture 
+create or replace view mse_pom as SELECT 
+    min(specimen_event_id) specimen_event_id ,
+    COLLECTION_OBJECT_ID 
+FROM 
+     specimen_event
+WHERE
+    specimen_event_type = 'place of manufacture'
+group by 
+    COLLECTION_OBJECT_ID
+;
+
+
+
+
+-- 2: not-unaccepted with coordinates
 create or replace view mse_nua_coord as  SELECT 
-    min(specimen_event_id)  specimen_event_id ,
+    min(specimen_event_id) specimen_event_id ,
     COLLECTION_OBJECT_ID 
 FROM 
      specimen_event ,
@@ -66,13 +105,55 @@ WHERE
     specimen_event.collecting_event_id=collecting_event.collecting_event_id and
     collecting_event.locality_id=locality.locality_id and
     locality.dec_lat is not null and
-    specimen_event_type != 'unaccepted place of collection'
+    specimen_event_type != 'unaccepted place of collection' and
+    COLLECTION_OBJECT_ID not in (select COLLECTION_OBJECT_ID from mse_pom)
 group by 
     COLLECTION_OBJECT_ID
 ;
+/*
+performance sucks, try a different join
+	select * from ( select * from mse_nua_coord order by dbms_random.value ) where rownum <= 100;
+     	select * from ( select * from mse_nua_nocoord order by dbms_random.value ) where rownum <= 100;
+     	select * from ( select * from map_specimen_event order by dbms_random.value ) where rownum <= 100;
+     	select * from ( select * from specimen_event order by dbms_random.value ) where rownum <= 100;
+     
+select count(*) from mse_nua_coord;
 
---  for specimens not in that, not-unaccepted without coordinats
+  COUNT(*)
+----------
+   1566007
 
+1 row selected.
+
+Elapsed: 00:00:04.70
+
+
+create or replace view mse_nua_coord as  SELECT 
+    min(specimen_event.specimen_event_id) specimen_event_id ,
+    specimen_event.COLLECTION_OBJECT_ID 
+FROM 
+     specimen_event ,
+     collecting_event,
+     locality,
+     mse_pom
+WHERE
+    specimen_event.collecting_event_id=collecting_event.collecting_event_id and
+    collecting_event.locality_id=locality.locality_id and
+    locality.dec_lat is not null and
+    specimen_event.specimen_event_type != 'unaccepted place of collection' and
+    specimen_event.COLLECTION_OBJECT_ID=mse_pom.COLLECTION_OBJECT_ID (+) and
+    mse_pom.COLLECTION_OBJECT_ID is null
+group by 
+    specimen_event.COLLECTION_OBJECT_ID
+;
+
+00:00:17.33
+
+bah nevermind....
+
+ */
+
+-- 3: not-unaccepted withOUT coordinates
 create or replace view mse_nua_nocoord as  SELECT 
     min(specimen_event_id)  specimen_event_id ,
     COLLECTION_OBJECT_ID 
@@ -84,45 +165,76 @@ WHERE
     specimen_event.collecting_event_id=collecting_event.collecting_event_id and
     collecting_event.locality_id=locality.locality_id and
     locality.dec_lat is null and
-    COLLECTION_OBJECT_ID not in (select COLLECTION_OBJECT_ID from mse_nua_coord) and
+    COLLECTION_OBJECT_ID not in (
+    	select COLLECTION_OBJECT_ID from mse_pom
+    	UNION
+    	select COLLECTION_OBJECT_ID from mse_nua_coord
+    ) and
     specimen_event_type != 'unaccepted place of collection'
 group by 
     COLLECTION_OBJECT_ID
 ;
---   for specimens not in those two, whatever's left
 
-create or replace view mse_therest as  SELECT 
+-- 4: leftovers, everything with not-unaccepted events that we haven't found yet
+create or replace view mse_nua_nounaccd as  SELECT 
     min(specimen_event_id)  specimen_event_id ,
     COLLECTION_OBJECT_ID 
 FROM 
-     specimen_event
+     specimen_event ,
+     collecting_event,
+     locality
 WHERE
+    specimen_event.collecting_event_id=collecting_event.collecting_event_id and
+    collecting_event.locality_id=locality.locality_id and
+    locality.dec_lat is null and
     COLLECTION_OBJECT_ID not in (
-        select COLLECTION_OBJECT_ID from mse_nua_coord
-        union
-        select COLLECTION_OBJECT_ID from mse_nua_nocoord
-     )
+    	select COLLECTION_OBJECT_ID from mse_pom
+    	UNION
+    	select COLLECTION_OBJECT_ID from mse_nua_coord
+    	UNION
+    	select COLLECTION_OBJECT_ID from mse_nua_nocoord
+    ) and
+    specimen_event_type != 'unaccepted place of collection'
 group by 
     COLLECTION_OBJECT_ID
-  ;
-  
- create or replace view map_specimen_event as select 
-  specimen_event_id,
-  COLLECTION_OBJECT_ID
- from 
-  mse_nua_coord
- union select 
-  specimen_event_id,
-  COLLECTION_OBJECT_ID
- from 
-  mse_nua_nocoord
- union select 
-  specimen_event_id,
-  COLLECTION_OBJECT_ID
- from 
-  mse_therest
+;
+
+
+
+create or replace view map_specimen_event as 
+	select specimen_event_id, COLLECTION_OBJECT_ID from mse_pom
+	UNION
+	select specimen_event_id, COLLECTION_OBJECT_ID from mse_nua_coord
+	UNION
+	select specimen_event_id, COLLECTION_OBJECT_ID from mse_nua_nocoord
+	UNION
+	select specimen_event_id, COLLECTION_OBJECT_ID from mse_therest
  ;
   
+ 
+ 
+ ----- test
+ 
+ select stale_flag,count(*) from flat group by stale_flag;
+ 
+ update flat set stale_flag=1 where collection_object_id=12;
+ 
+ exec is_flat_stale;
+ 
+ -- old model: 00:00:00.76
+ -- new model: 00:00:01.06
+ 
+  update flat set stale_flag=1 where rownum < 500;
+  
+  -- old: 00:00:24.33
+  -- new: 00:07:23.58
+  -- GASP! Turn off schedule, try again
+  -- new2: 00:07:15.88
+  
+  
+  
+
+ 
  -- test
  -- select dec_lat ,stale_flag from flat where guid='UAMb:Herb:30408';
  -- nada
