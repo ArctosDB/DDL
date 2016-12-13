332d4902bd176bd2165ce78f6987e3517aed66a0
dustymc <dustymc@gmail.com>
2016-12-13 07:58:43 -0800

    Signed-off-by: dustymc <dustymc@gmail.com>

---
+++ migration/_____projectmarkdown.sql
@@ -0,0 +1,3 @@
+alter table project drop constraint CK_PROJECT_DESCRIPTION_NOPRINT;
+
+-- permissions for info/mdeditor.cfm
---
+++ migration/test.sql
@@ -0,0 +1,15 @@
+This is a test.
+
+it should say something about code.
+
+and have some sql and stuff.
+
+the name will usually be a version number.
+
+Can/should we close all issues with release notes?
+
+Maybe.
+
+Closes ArctosDB/arctos/#997 with bla bla blabitty
+
+Fixed ArctosDB/arctos/#998 with woopty something bla
