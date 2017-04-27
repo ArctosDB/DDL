abdb935ecb86f04518c01a26bbff87725942e29e
dustymc <dustymc@gmail.com>
2017-04-27 10:35:16 -0700

    Signed-off-by: dustymc <dustymc@gmail.com>

--- triggers/uam_ct_triggers/ctabundance.sql
+++ triggers/uam_ct_triggers/ctabundance.sql
@@ -7,22 +7,24 @@ DECLARE
     v attributes.attribute_value%TYPE;
     o collection.collection_cde%TYPE;
 BEGIN
-    v := :old.abundance;
-    o := :OLD.COLLECTION_CDE;
-
-    FOR r IN (
-        SELECT COUNT(*) c
-        FROM attributes, cataloged_item, collection
-        WHERE attributes.collection_object_id = cataloged_item.collection_object_id
-        AND cataloged_item.collection_id = collection.collection_id
-        AND attribute_type = 'abundance'
-        AND attribute_value = v
-        AND collection.collection_cde = o
-    ) LOOP
-        IF r.c > 0 THEN
-            raise_application_error(
-                -20001,
-                v || ' (c = ' || r.c || ') is used in attributes for collection type ' || o);
-        END IF;
-    END LOOP;
+    if :OLD.abundance != :NEW.abundance then
+		v := :old.abundance;
+	    o := :OLD.COLLECTION_CDE;
+	
+	    FOR r IN (
+	        SELECT COUNT(*) c
+	        FROM attributes, cataloged_item, collection
+	        WHERE attributes.collection_object_id = cataloged_item.collection_object_id
+	        AND cataloged_item.collection_id = collection.collection_id
+	        AND attribute_type = 'abundance'
+	        AND attribute_value = v
+	        AND collection.collection_cde = o
+	    ) LOOP
+	        IF r.c > 0 THEN
+	            raise_application_error(
+	                -20001,
+	                v || ' (c = ' || r.c || ') is used in attributes for collection type ' || o);
+	        END IF;
+	    END LOOP;
+	end if;
 END;
