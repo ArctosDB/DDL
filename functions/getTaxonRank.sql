a28d3c3bfc96d5c603c4283d8865283c18bf55cf
dustymc <dustymc@gmail.com>
2017-12-04 10:24:23 -0800

    Signed-off-by: dustymc <dustymc@gmail.com>

---
+++ functions/getTaxonRank.sql
@@ -0,0 +1,45 @@
+/*
+ Attempt to get DWC:taxonRank (http://rs.tdwg.org/dwc/terms/taxonRank) for iDigBio
+ Ignore:
+ 	scientific_name, which should always be a replication of the lowest classification term (which is not necessarily ranked)
+ 	display_name, which is sometimes a replication of the lowest classification term (which is not necessarily ranked)
+ 	
+ Match:
+ 	exact ID string <--> taxon_term.term
+ 
+ */
+
+CREATE OR REPLACE function getTaxonRank(collobjid IN number)
+    return varchar2
+    as
+    tterm  varchar2(4000);
+  begin
+	select
+		taxon_term.term_type
+	into
+		tterm
+	from
+		taxon_term,
+		identification,
+		identification_taxonomy,
+		collection,
+		cataloged_item
+	where
+		taxon_term.term=identification.scientific_name and
+		taxon_term.source=collection.PREFERRED_TAXONOMY_SOURCE and
+		identification.identification_id=identification_taxonomy.identification_id and
+		identification_taxonomy.taxon_name_id=taxon_term.taxon_name_id and
+		cataloged_item.collection_id=collection.collection_id and
+		cataloged_item.collection_object_id=identification.collection_object_id and
+		identification.accepted_id_fg=1 and
+		taxon_term.term_type != 'scientific_name' and
+		taxon_term.term_type != 'display_name' and
+		cataloged_item.collection_object_id = collobjid
+	;
+    return tterm;
+    EXCEPTION
+    	when others then
+        	tterm := 'error!: ' || sqlerrm;
+        	return trim(tterm);
+  end;
+ /
