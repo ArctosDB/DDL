fcfa773e20c123a87785c64a0b981263dccf1683
dustymc <dustymc@gmail.com>
2018-03-08 13:27:59 -0800

    .
    Signed-off-by: dustymc <dustymc@gmail.com>

--- functions/escape_json.sql
+++ functions/escape_json.sql
@@ -14,8 +14,6 @@ CREATE OR REPLACE function escape_json (s in varchar2)
 
 sho err;
 
-select escape_json('The museum tag for this specimen also had the code "211 7/9" recorded on it.') from dual;
-
 
 create public synonym escape_json for escape_json;
 grant execute on escape_json to public;
