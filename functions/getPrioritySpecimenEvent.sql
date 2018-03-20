87d9d728ab8cce13afc6c0c16014ced226171cc0
dustymc <dustymc@gmail.com>
2018-03-19 21:41:10 -0700

    update getPrioritySpecimenEvent
    Signed-off-by: dustymc <dustymc@gmail.com>

--- functions/getPrioritySpecimenEvent.sql
+++ functions/getPrioritySpecimenEvent.sql
@@ -1,3 +1,88 @@
+ /*
+	NEW AND HOT!!
+		- old-n-busted below, in case
+	
+	rank various stuff, sum up the ranks, take the winner
+	
+	* specimen_event_type
+		* heavily favor "manufacture" - this should always win when present
+		* slightly favor 'collect' 
+	* coordiantes
+    		* slight favor for having coordinates
+	* verificationstatus
+		* this should basically control the winner when there's no 'manufacture' event type 
+
+  */
+CREATE OR REPLACE FUNCTION getPrioritySpecimenEvent(coid IN number)
+RETURN number
+AS
+	sid number;
+BEGIN
+  select 
+  	specimen_event_id 
+  into 
+  	sid
+  from (
+  	select 
+    		specimen_event_id,
+    		--specimen_event_type,
+    		--verificationstatus,
+    		-- sum up the points columns
+    		lpt + sept + vspt ttlpts
+    	from (
+    		select
+      		specimen_event.specimen_event_id,
+      		--specimen_event.specimen_event_type,
+      		--specimen_event.verificationstatus,
+      		decode(locality.dec_lat,null,0,1) lpt,
+			decode(specimen_event.specimen_event_type,
+				'manufacture',10,
+				'collection',1,
+				0
+  			) sept,
+			decode(specimen_event.verificationstatus,
+				'verified and locked',5,
+				'accepted',4,
+				'unverified',0,
+				'unaccepted',-1,
+        		0) vspt
+ 		from
+      		specimen_event,
+      		collecting_event,
+      		locality
+    		where
+      		specimen_event.collecting_event_id=collecting_event.collecting_event_id and
+      		collecting_event.locality_id=locality.locality_id and
+      		--specimen_event.verificationstatus != 'unaccepted' and 
+      		specimen_event.collection_object_id=coid
+     ) 
+	order by  
+		ttlpts desc
+	) where rownum=1;
+	return sid;
+end;
+/
+
+
+
+
+CREATE or replace PUBLIC SYNONYM getPrioritySpecimenEvent FOR getPrioritySpecimenEvent;
+GRANT EXECUTE ON getPrioritySpecimenEvent TO PUBLIC;
+
+
+
+select getPrioritySpecimenEvent2(23817647) from dual;
+
+
+/*
+ 
+ 
+
+old-n-busted follows
+
+
+*/
+
 /*
  	Finds "best" specimen event, where "best" is ordered by
  		event type is place of manufacture
@@ -23,11 +108,11 @@ BEGIN
 		where
 			specimen_event.collecting_event_id=collecting_event.collecting_event_id and
 			collecting_event.locality_id=locality.locality_id and
-			specimen_event.specimen_event_type != 'unaccepted place of collection' and 
+			--specimen_event.verificationstatus != 'unaccepted' and 
 			specimen_event.collection_object_id=coid
 		order by began_date ASC
 	) loop
-		if r.specimen_event_type = 'place of manufacture' then
+		if r.specimen_event_type = 'manufacture' then
 			tp:=1;
 			if tp < p then
 				p:=tp;
---
+++ migration/_____mediaJSONSpecResults.sql
@@ -0,0 +1,8 @@
+rebuild getJsonMediaUriBySpecimen
+rebuild specimenresult.js
+switch the min in
+
+build/switch ajax.min
+
+add info/flat_status.cfm to roles
+
