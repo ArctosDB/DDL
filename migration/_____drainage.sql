e84660b824bdcf3442c2aff66b41fc5065eddc49
dustymc <dustymc@gmail.com>
2018-04-26 21:21:37 -0700

    .
    Signed-off-by: dustymc <dustymc@gmail.com>

--- migration/_____drainage.sql
+++ migration/_____drainage.sql
@@ -108,5 +108,127 @@ insert into ssrch_field_doc (
  );
  
  
- select spec_locality from flat where guid like 'MSB:Fish%' group by spec_locality order by spec_locality;
+ 
+ 
+ --- now data
+ create table temp_msb_f_d as 
+ select
+ 	higher_geog,
+ 	--locality_remarks,
+ 	--instr(locality_remarks,'Drainage'),
+ 	substr(locality_remarks,0,instr(locality_remarks,'Drainage')+7) drainage
+ from
+ 	geog_auth_rec,
+ 	locality,
+ 	collecting_event,
+ 	specimen_event,
+ 	cataloged_item,
+ 	collection
+ where
+ 	geog_auth_rec.geog_auth_rec_id=locality.geog_auth_rec_id and
+ 	locality.locality_id=collecting_event.locality_id and
+ 	collecting_event.collecting_event_id=specimen_event.collecting_event_id and
+ 	specimen_event.collection_object_id=cataloged_item.collection_object_id and
+ 	cataloged_item.collection_id=collection.collection_id and
+ 	collection.guid_prefix='MSB:Fish' and
+ 	locality_remarks like '%Drainage%'
+ group by
+ 	higher_geog,
+ 	substr(locality_remarks,0,instr(locality_remarks,'Drainage')+7)
+ ;
+ 
+ select * from temp_msb_f_d order by drainage,higher_geog;
+ 
+ 
+ create table temp_msb_f_do as select distinct drainage from temp_msb_f_d;
+ 
+ spec_locality from flat where guid like 'MSB:Fish%' group by spec_locality order by spec_locality;
+ 
+ 
+ delete from temp_msb_f_d where drainage='Fish Hatchery Drainage';
+ 
+ 
+ select distinct drainage, replace(drainage,' Drainage') from temp_msb_f_d;
+ 
+ update temp_msb_f_d set drainage=replace(drainage,' Drainage');
+ 
+  select distinct drainage from temp_msb_f_d;
+  
+  alter table temp_msb_f_d add wiki varchar2(255) ;
+  update temp_msb_f_d set wiki='https://en.wikipedia.org/wiki/Mississippi_River_System' where drainage='Mississippi River';
+  update temp_msb_f_d set wiki='https://en.wikipedia.org/wiki/Arkansas_River' where drainage='Arkansas River';
+  update temp_msb_f_d set wiki='Atlantic Ocean Slope' where drainage='https://en.wikipedia.org/wiki/Atlantic_Ocean';
+  update temp_msb_f_d set wiki='https://en.wikipedia.org/wiki/Gila_River' where drainage='Gila River';
+  update temp_msb_f_d set wiki='https://en.wikipedia.org/wiki/Missouri_River' where drainage='Missouri River';
+  update temp_msb_f_d set wiki='https://en.wikipedia.org/wiki/Guzm%C3%A1n_Basin' where drainage='Guzman Basin';
+  update temp_msb_f_d set wiki='https://en.wikipedia.org/wiki/Rio_Grande' where drainage='Rio Grande';
+  update temp_msb_f_d set wiki='https://en.wikipedia.org/wiki/Canadian_River' where drainage='Canadian River';
+  update temp_msb_f_d set wiki='https://en.wikipedia.org/wiki/Pacific_Slope' where drainage='Pacific Ocean Slope';
+  update temp_msb_f_d set wiki='https://en.wikipedia.org/wiki/San_Juan_River_(Colorado_River_tributary)' where drainage='San Juan River';
+  update temp_msb_f_d set wiki='https://en.wikipedia.org/wiki/Zuni_River' where drainage='Zuni River';
+  update temp_msb_f_d set wiki='https://en.wikipedia.org/wiki/Great_Basin' where drainage='Great Basin';
+  update temp_msb_f_d set wiki='https://en.wikipedia.org/wiki/Colorado_River' where drainage='Colorado River';
+  update temp_msb_f_d set wiki='https://en.wikipedia.org/wiki/Arctic_Ocean' where drainage='Arctic Ocean';
+  update temp_msb_f_d set wiki='https://en.wikipedia.org/wiki/Pecos_River' where drainage='Pecos River';
+  update temp_msb_f_d set wiki='https://en.wikipedia.org/wiki/Gulf_of_Mexico' where drainage='Gulf of Mexico';
+  update temp_msb_f_d set wiki='https://en.wikipedia.org/wiki/Great_Lakes' where drainage='Great Lakes';
+  update temp_msb_f_d set wiki='https://en.wikipedia.org/wiki/Tularosa_Basin' where drainage='Tularosa Basin';
+  update temp_msb_f_d set wiki='https://en.wikipedia.org/wiki/Atlantic_Ocean' where drainage='Atlantic Ocean Slope';
+  update temp_msb_f_d set wiki='xxx' where drainage='xxxx';
+  
+  
+  select * from temp_msb_f_d where wiki is null;
+  
+  declare
+  	isl varchar2(255);
+  begin
+	  for r in (select distinct higher_geog,drainage, wiki from temp_msb_f_d) loop
+	  	dbms_output.put_line(r.drainage);
+	  	dbms_output.put_line(r.higher_geog);
+	  	dbms_output.put_line(r.wiki);
+		for s in (select * from geog_auth_rec where higher_geog=r.higher_geog ) loop
+			if s.island is not null then
+				isl:='*' || s.island;
+			else
+				isl:=null;
+			end if;
+	  		dbms_output.put_line(s.geog_auth_rec_id);
+	  		insert into geog_auth_rec(
+	  			GEOG_AUTH_REC_ID,
+	  			CONTINENT_OCEAN,
+	  			COUNTRY,
+	  			STATE_PROV,
+	  			COUNTY,
+	  			QUAD,
+	  			FEATURE,
+	  			ISLAND,
+	  			ISLAND_GROUP,
+	  			SEA,
+	  			VALID_CATALOG_TERM_FG,
+	  			SOURCE_AUTHORITY,
+	  			DRAINAGE				
+	  		) values (
+	  			sq_GEOG_AUTH_REC_ID.nextval,
+	  			s.CONTINENT_OCEAN,
+	  			s.COUNTRY,
+	  			s.STATE_PROV,
+	  			s.COUNTY,
+	  			s.QUAD,
+	  			s.FEATURE,
+	  			isl,
+	  			s.ISLAND_GROUP,
+	  			s.SEA,
+	  			1,
+	  			r.wiki,
+	  			r.DRAINAGE		
+	  		);
+	  		
+	  	end loop;
+	  end loop;
+  end ;
+  /
+  
+  
+  
+
  
