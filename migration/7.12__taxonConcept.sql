6e7335c9bfad35fd6a8655d3a67727a279901d37
dustymc <dustymc@gmail.com>
2019-11-04 08:00:49 -0800

    .
    Signed-off-by: dustymc <dustymc@gmail.com>

--- migration/7.12__taxonConcept.sql
+++ migration/7.12__taxonConcept.sql
@@ -119,3 +119,76 @@ alter table taxon_concept modify concept_label not null;
 
 http://arctos.database.museum/editTaxonomy.cfm?action=editnoclass&taxon_name_id=
 http://arctos.database.museum/publication/1000138
+
+
+
+
+
+
+
+
+
+--------------------- PG
+
+
+drop table taxon_concept;
+create  sequence sq_taxon_concept_id;
+GRANT USAGE, SELECT ON SEQUENCE sq_taxon_concept_id TO public;
+
+create table taxon_concept (
+	taxon_concept_id bigint  not null,
+	taxon_name_id bigint not null,
+	publication_id bigint not null
+);
+
+grant all on taxon_concept to manage_taxonomy;
+grant select on taxon_concept to public;
+
+
+alter table taxon_concept add constraint pktaxon_concept primary key (taxon_concept_id);
+ALTER TABLE taxon_concept ADD CONSTRAINT fk_concept_name  FOREIGN KEY (taxon_name_id)  REFERENCES taxon_name(taxon_name_id);
+ALTER TABLE taxon_concept ADD CONSTRAINT fk_concept_pub  FOREIGN KEY (publication_id)  REFERENCES publication(publication_id);
+
+drop table taxon_concept_rel;
+create sequence sq_taxon_concept_rel_id;
+GRANT USAGE, SELECT ON SEQUENCE sq_taxon_concept_rel_id TO public;
+
+create table taxon_concept_rel (
+	taxon_concept_rel_id bigint not null,
+	from_taxon_concept_id bigint not null,
+	to_taxon_concept_id bigint not null,
+	relationship varchar(255)  not null,
+	according_to_publication_id bigint  not null
+);
+
+create table cttaxon_concept_relationship (
+	relationship varchar(30) not null,
+	description varchar(4000) not null
+);
+
+grant all on cttaxon_concept_relationship to manage_codetables;
+grant select on cttaxon_concept_relationship to public;
+
+alter table cttaxon_concept_relationship add constraint pkcttaxon_concept_relationship primary key (relationship);
+
+
+
+grant all on taxon_concept_rel to manage_taxonomy;
+
+alter table taxon_concept_rel add constraint pktaxon_concept_rel primary key (taxon_concept_rel_id);
+ALTER TABLE taxon_concept_rel ADD CONSTRAINT fk_tcrel_ftc  FOREIGN KEY (from_taxon_concept_id)  REFERENCES taxon_concept(taxon_concept_id);
+ALTER TABLE taxon_concept_rel ADD CONSTRAINT fk_tcrel_ttc  FOREIGN KEY (to_taxon_concept_id)  REFERENCES taxon_concept(taxon_concept_id);
+ALTER TABLE taxon_concept_rel ADD CONSTRAINT fk_tcrel_pub  FOREIGN KEY (according_to_publication_id)  REFERENCES publication(publication_id);
+ALTER TABLE taxon_concept_rel ADD CONSTRAINT fk_tcrel_relshp  FOREIGN KEY (relationship)  REFERENCES cttaxon_concept_relationship(relationship);
+
+
+-- this is hard to display and will be confusing, so...
+
+alter table taxon_concept add concept_label varchar(255);
+
+update taxon_concept set concept_label='<i>Claytonia scammaniana</i> Hulten <i>sensu</i> Hulton 1942';
+alter table taxon_concept alter concept_label not null;
+
+
+http://arctos.database.museum/editTaxonomy.cfm?action=editnoclass&taxon_name_id=
+http://arctos.database.museum/publication/1000138
